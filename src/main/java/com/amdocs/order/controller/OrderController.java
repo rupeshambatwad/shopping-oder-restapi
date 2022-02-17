824d7b3db4535c0bbc5064e9513cfacb792a23f8
rupeshambatwad <30683368+rupeshambatwad@users.noreply.github.com>
2022-02-17 11:33:18 +0530

    updateOrder
    updateOrder Functionality

--- src/main/java/com/amdocs/order/controller/OrderController.java
+++ src/main/java/com/amdocs/order/controller/OrderController.java
@@ -3,12 +3,9 @@ package com.amdocs.order.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import com.amdocs.order.dto.OrderDto;
 import com.amdocs.order.service.OrderService;
@@ -44,8 +41,9 @@ public class OrderController {
 	{
 		
 	}
-	public OrderDto updateOrder(long id,OrderDto orderDto)
+	@PutMapping("/{id}")
+	public ResponseEntity<OrderDto> updateOrder(@PathVariable long id,@RequestBody OrderDto orderDto)
 	{
-		return null;
+		return new ResponseEntity<>(orderService.updateOrder(id, orderDto), HttpStatus.OK);
 	}
 }
